0b124831ebc40f171fd16fc153c29a039e036f8e
Aaron Schuster <aaronjschuster@gmail.com>
2023-03-31 14:03:28 -0400

    write checkUsernameExists middleware

--- api/auth/auth-middleware.js
+++ api/auth/auth-middleware.js
@@ -1,5 +1,7 @@
 const { JWT_SECRET } = require("../secrets"); // use this secret!
 
+const User = require("../users/users-model");
+
 const restricted = (req, res, next) => {
   next();
 
@@ -36,7 +38,15 @@ const only = role_name => (req, res, next) => {
 
 
 const checkUsernameExists = (req, res, next) => {
-  next();
+  User.findBy({username: req.body.username})
+    .then( user => {
+      if(user[0]) next();
+      else next({
+        status: 401,
+        message: "Invalid credentials."
+      })
+    })
+    .catch(next);
   /*
     If the username in req.body does NOT exist in the database
     status 401
